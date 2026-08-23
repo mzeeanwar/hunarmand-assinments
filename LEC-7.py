0e8fc5a624cf594b62193113d09c051529bf2036
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-08-24 00:47:57 +0500

    Building Q/A Function + Task Practice
    Add function to calculate rectangle area

---
+++ LEC-7.py
@@ -0,0 +1,8 @@
+# Function to calculate area of a rectangle
+def calculate_area(length, width):
+    return length * width
+
+# Calling the function with different values
+print("Area (5 x 3):", calculate_area(5, 3))   # 15
+print("Area (10 x 2):", calculate_area(10, 2)) # 20
+print("Area (7 x 4):", calculate_area(7, 4))   # 28
