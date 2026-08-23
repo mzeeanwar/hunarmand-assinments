e450c88a7d01109ab4f8fc128046ab771cec6919
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-08-24 00:47:11 +0500

    Python Advance Concepts – Inputs
    Python Advance Concepts – InputsAdd user input program for name, age, and GPA

---
+++ LEC-6.py
@@ -0,0 +1,9 @@
+# Program to take user input and display details
+
+# Taking inputs
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gpa = float(input("Enter your GPA: "))
+
+# Displaying the message
+print(f"Hello {name}, your age is {age} and your GPA is {gpa}")
