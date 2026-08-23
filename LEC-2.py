aa110fa87bdb4494a27ad665fe952f61caff88b4
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-08-24 00:38:56 +0500

    Add variable declarations and type printing

---
+++ LEC-2.py
@@ -0,0 +1,11 @@
+# Creating variables of different types
+num_int = 10          # int
+num_float = 3.14      # float
+text_str = "Hello"    # str
+flag_bool = True      # bool
+
+# Printing each variable and its type
+print(num_int, type(num_int))       # Output: 10 <class 'int'>
+print(num_float, type(num_float))   # Output: 3.14 <class 'float'>
+print(text_str, type(text_str))     # Output: Hello <class 'str'>
+print(flag_bool, type(flag_bool))   # Output: True <class 'bool'>
