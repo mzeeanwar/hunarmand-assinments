22df02836976837b070fba1fd25651dea57626a3
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-08-26 01:29:06 +0500

    Add examples for factorial and random number generation

--- 2nd-assignment/LEC-16.py
+++ 2nd-assignment/LEC-16.py
@@ -6,3 +6,10 @@ import random
 # --- Using math.sqrt() ---
 num = 25
 print("Square root of", num, "is:", math.sqrt(num))
+# --- Using math.factorial() ---
+n = 5
+print("Factorial of", n, "is:", math.factorial(n))
+
+# --- Using random.randint() ---
+rand_num = random.randint(1, 100)
+print("Random number between 1 and 100:", rand_num)
